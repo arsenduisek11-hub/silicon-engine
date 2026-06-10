e06e480fb5e5aa06efe8184ff7563a6de3450d98
arsen duisek Fullstack - all web languages and c++ java kotlin <arsenduisek11@gmail.com>
2026-06-10 13:48:58 +0500

    Create vertex.glsl

---
+++ shaders/vertex.glsl
@@ -0,0 +1,13 @@
+
+layout (location = 0) in vec2 aPos;
+layout (location = 1) in vec2 aTexCoords;
+
+out vec2 TexCoords;
+
+uniform mat4 model;
+uniform mat4 projection;
+
+void main() {
+    TexCoords = aTexCoords;
+    gl_Position = projection * model * vec4(aPos, 0.0, 1.0);
+}
